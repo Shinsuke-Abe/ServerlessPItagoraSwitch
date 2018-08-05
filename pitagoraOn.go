296c72ad579ca134580daa2351a5032fc05b2b0d
Shinsuke-Abe <mao.instantlife@gmail.com>
2018-08-05 11:07:59 +0900

    get parameter ifttt key from ssm

--- pitagoraOn.go
+++ pitagoraOn.go
@@ -4,20 +4,50 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-}
+// type MyEvent struct {
+// 	Name serialNumber   `json:"serialNumber"`
+// 	Name clickType      `json:"clickType"`
+// 	Name batteryVoltage `json:batteryVoltage`
+// }
 
 type MyResponse struct {
 	Message string `json:"Answer:"`
 }
 
-func pitagoraOn(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("Hello %s", event.Name)}, nil
+//event MyEvent
+func pitagoraOn() (MyResponse, error) {
+	sess, sessionError := session.NewSession()
+	if sessionError != nil {
+		panic(sessionError)
+	}
+
+	service := ssm.New(sess)
+
+	param := ssm.GetParameterInput{
+		Name:           aws.String("pitagora-switch-ifttt-key"),
+		WithDecryption: aws.Bool(true),
+	}
+
+	keyOutput, keyError := service.GetParameter(&param)
+	if keyError != nil {
+		panic(keyError)
+	}
+
+	url := fmt.Sprintf(
+		"https://maker.ifttt.com/trigger/Pi-ta-go-ra-Switch/with/key/%s",
+		*keyOutput.Parameter.Value)
+
+	fmt.Println(url)
+
+	return MyResponse{Message: fmt.Sprintf("Hello %s", "shinsuke")}, nil
 }
 
 func main() {
+
 	lambda.Start(pitagoraOn)
 }
