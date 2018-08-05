5274680916f6908f7faceb0e6b0a39e53d6efd33
Shinsuke-Abe <mao.instantlife@gmail.com>
2018-08-05 13:16:21 +0900

    Pitagora switch on!

--- pitagoraOn.go
+++ pitagoraOn.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
+
+	"net/http"
 )
 
-// type MyEvent struct {
-// 	Name serialNumber   `json:"serialNumber"`
-// 	Name clickType      `json:"clickType"`
-// 	Name batteryVoltage `json:batteryVoltage`
-// }
+type MyEvent struct {
+	SerialNumber   string `json:"serialNumber"`
+	ClickType      string `json:"clickType"`
+	BatteryVoltage string `json:"batteryVoltage"`
+}
 
 type MyResponse struct {
 	Message string `json:"Answer:"`
 }
 
-//event MyEvent
-func pitagoraOn() (MyResponse, error) {
+func pitagoraOn(event MyEvent) (MyResponse, error) {
 	sess, sessionError := session.NewSession()
 	if sessionError != nil {
 		panic(sessionError)
@@ -42,12 +44,20 @@ func pitagoraOn() (MyResponse, error) {
 		"https://maker.ifttt.com/trigger/Pi-ta-go-ra-Switch/with/key/%s",
 		*keyOutput.Parameter.Value)
 
-	fmt.Println(url)
+	values := fmt.Sprintf(
+		"{Value1: serialNumber is %s, Value2: clickType is %s, Value3: batteryVoltage is %s}",
+		event.SerialNumber,
+		event.ClickType,
+		event.BatteryVoltage)
 
-	return MyResponse{Message: fmt.Sprintf("Hello %s", "shinsuke")}, nil
+	fmt.Println(url)
+	fmt.Println(values)
+
+	http.Post(url, "text/json", strings.NewReader(values))
+
+	return MyResponse{Message: fmt.Sprintf("Pitagora Switch kicked!!!\n%s", values)}, nil
 }
 
 func main() {
-
 	lambda.Start(pitagoraOn)
 }
